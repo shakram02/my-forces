7ba537592828ec0eeca912e1eabe60986540a985
shakram02 <ahmedhamdyau@gmail.com>
2017-07-05 10:15:15 +0200

    fire again - optimal

--- fire-again/fire-again-35-c.cpp
+++ fire-again/fire-again-35-c.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
+#include <iomanip>
 using namespace std;
 typedef pair<int, int> point_t;
 
-int forest[2000][2000] = {0};
-int max_depth = 0;
 int rows, count, init_burning;
 point_t last_point;
-set<point_t> start_points;
-
-void get_near(pair<int, int> p, set<pair<int, int>> &n);
-bool are_all_burnt(vector<pair<int, int>> &o);
+vector<point_t> start_points;
 
 #ifndef ONLINE_JUDGE
-void print(set<pair<int, int>> &v)
+void print(vector<vector<int>> &f)
 {
-    for (auto const &x : v)
+    cout << endl;
+    for (int i = 0; i < rows; i++)
     {
-        cout << "(" << x.first + 1 << "," << x.second + 1 << ") ";
+        for (int j = 0; j < count; j++)
+        {
+            cout << setw(3) << f[i][j] << " ";
+        }
+        cout << endl;
     }
     cout << endl;
 }
 #endif
-
-pair<int, int> get_first(set<pair<int, int>> &s)
-{
-    pair<int, int> head = *start_points.begin();
-    start_points.erase(head);
-
-    return head;
-}
 
 int main()
 {
@@ -42,99 +34,49 @@ int main()
 
     cin >> rows >> count >> init_burning;
 
+    vector<vector<int>> forest(rows, vector<int>(count, rows + count));
+
     for (int i = 0; i < init_burning; i++)
     {
         int x, y;
         cin >> x >> y;
         y--;
         x--;
-        start_points.insert(make_pair(x, y));
+        start_points.push_back(make_pair(x, y));
     }
 
-    last_point = *start_points.begin(); // Base case
-    set<point_t> to_visit;
-
+    last_point = start_points[0]; // Base case
+    int max_depth = 0;
     // Burn the first generation (start points), then the second (to_visit), ...etc
-    while (!start_points.empty())
+    for (auto const &p : start_points)
     {
-        point_t pivot = get_first(start_points);
-
-        if (forest[pivot.first][pivot.second])
+        for (int i = 0; i < rows; i++)
         {
-            // If it's burnt, I mut've calculated the neighbors
-            continue;
-        }
-
-#ifndef ONLINE_JUDGE
-        cout << "Burning: " << pivot.first + 1 << "," << pivot.second + 1 << endl;
-        // print(n);
-#endif
-
-        set<point_t> n;
-        get_near(pivot, n);
-        forest[pivot.first][pivot.second] = 1;
-        last_point = pivot;
-
-        for (auto const &p : n)
-        {
-            if (forest[p.first][p.second])
+            for (int j = 0; j < count; j++)
             {
-                continue;
+                int val = abs(i - p.first) + abs(j - p.second);
+                forest[i][j] = min(val, forest[i][j]);
             }
-            to_visit.insert(p);
-        }
-
-        if (start_points.empty())
-        {
-#ifndef ONLINE_JUDGE
-            cout << endl;
-#endif
-            for (auto const &x : to_visit)
-            {
-                if (forest[x.first][x.second])
-                {
-                    continue;
-                }
-                start_points.insert(x);
-            }
-            to_visit.clear();
         }
     }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            if (forest[i][j] > max_depth)
+            {
+                max_depth = forest[i][j];
+                last_point = make_pair(i, j);
+            }
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    print(forest);
+    cout << "Max depth:" << max_depth << endl;
+#endif
 
     cout << last_point.first + 1 << " " << last_point.second + 1 << endl;
     return 0;
-}
-
-void get_near(pair<int, int> p, set<pair<int, int>> &n)
-{
-    int r_idx = p.first;
-    int c_idx = p.second;
-    int start_row, end_row, start_col, end_col;
-
-    start_row = (r_idx == 0) ? r_idx : r_idx - 1;
-    end_row = (r_idx + 1 >= rows) ? r_idx : r_idx + 1;
-
-    start_col = (c_idx == 0) ? c_idx : c_idx - 1;
-    end_col = (c_idx + 1 >= count) ? c_idx : c_idx + 1;
-
-    for (int i = start_row; i <= end_row; i++)
-    {
-
-        if (i == r_idx)
-        {
-            continue;
-        }
-
-        n.insert(make_pair(i, c_idx));
-    }
-
-    for (int j = start_col; j <= end_col; j++)
-    {
-        if (j == c_idx)
-        {
-            continue;
-        }
-
-        n.insert(make_pair(r_idx, j));
-    }
 }
